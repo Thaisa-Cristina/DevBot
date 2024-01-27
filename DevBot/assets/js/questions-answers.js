6e07855216f838df763a6b2438e75c6d0a5bf4db
Thaisa Cristina <90491687+Thaisa-Cristina@users.noreply.github.com>
2024-01-27 19:18:32 -0300

    Add files via upload

---
+++ DevBot/assets/js/questions-answers.js
@@ -0,0 +1,38 @@
+const chatbotData = {
+  //Mensagens de apresentação do bot//
+  "Olá": "Olá! Eu sou o DevBot, seu assistente virtual para assuntos relacionados ao desenvolvimento de software. Como posso ajudar você hoje?",
+  "Olá, sou novo aqui!": "Bem-vindo ao DevBot! Estou aqui para fornecer dicas, responder dúvidas e oferecer orientações sobre a empolgante jornada do desenvolvimento de software. Como posso ser útil para você hoje?",
+  "Obrigado por sua ajuda, DevBot!": "De nada! Estou aqui para ajudar. Se tiver mais perguntas ou precisar de assistência, é só me chamar. 😊",
+
+  //Mensagens sobre Carreira em Desenvolvimento//
+  "Qual a importância de aprender a programar para quem está começando na carreira de desenvolvimento de software?": "A programação é a base fundamental da carreira em desenvolvimento de software. Aprender a programar permite a criação de soluções tecnológicas e abre portas para diversas oportunidades profissionais.",
+  //
+  "Como escolher a linguagem de programação ideal para começar?": "Escolha uma linguagem amigável para iniciantes, como Python ou JavaScript. Considere também os requisitos do mercado e a aplicação desejada.",
+  //
+  "O que são frameworks e qual a sua importância no desenvolvimento de software?": " Frameworks são conjuntos de ferramentas e convenções que facilitam o desenvolvimento. Eles aceleram o processo, fornecendo estruturas já prontas para diversas tarefas.",
+  //
+  "Como superar a ansiedade ao iniciar projetos de programação?": "Divida o projeto em partes menores, estabeleça metas realistas, peça ajuda quando necessário e lembre-se de que enfrentar desafios faz parte do aprendizado.",
+  //
+  "Qual a importância da prática constante na programação para iniciantes?": "A prática constante melhora as habilidades de codificação, familiariza com conceitos fundamentais e aumenta a confiança ao resolver problemas.",
+  //
+  "Como encontrar recursos de aprendizado online para programação?": "Utilize plataformas como Codecademy, freeCodeCamp, Udacity e Coursera. Além disso, explore documentações oficiais e fóruns de programação.",
+  //
+  "Quais são as etapas essenciais no desenvolvimento de um projeto de software?": "As etapas incluem análise de requisitos, design, codificação, testes e manutenção. Entender cada fase é crucial para desenvolver projetos de forma eficaz.",
+  //
+  "Como lidar com erros e bugs durante a programação?": "Veja os erros como oportunidades de aprendizado, utilize ferramentas de depuração, pesquise soluções online e, se necessário, peça ajuda em comunidades de desenvolvedores.",
+  //
+  "Qual a importância da documentação no desenvolvimento de software?": "Documentação clara facilita a compreensão do código, colaboração eficaz entre membros da equipe e manutenção do software ao longo do tempo.",
+   //
+   "Como criar um portfólio para mostrar projetos de programação?": "Utilize plataformas como GitHub para armazenar projetos, adicione descrições claras, destaque tecnologias utilizadas e demonstre o processo de resolução de problemas.",
+   //
+   "Quais são os principais desafios enfrentados por iniciantes na carreira de desenvolvimento de software?": "Desafios comuns incluem a curva de aprendizado, a ansiedade ao enfrentar problemas complexos e a necessidade de se manter atualizado com novas tecnologias.",
+   //
+   "Como conseguir oportunidades de estágio ou projetos freelance como iniciante?": "Construa um portfólio sólido, participe de comunidades online, faça networking, e esteja aberto a estágios ou projetos menores para ganhar experiência.",
+   //
+   "Qual a importância de participar de comunidades de desenvolvedores?": "Comunidades oferecem suporte, compartilhamento de conhecimento, oportunidades de networking e uma visão mais ampla sobre as práticas da indústria.",
+
+};
+
+
+
+
